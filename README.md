dcbd1f0e05ec969e4b54eb899464a2dcf27cc267
Ade <2270734250@qq.com>
2019-07-24 12:25:19 +0800

    更新版本

--- README.md
+++ README.md
@@ -24,3 +24,6 @@ npm run test
 ```
 npm run lint
 ```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,11 @@ module.exports = {
     // 修改 src 为 examples
     pages: {
       index: {
+        // page 的入口
         entry: 'examples/main.js',
+        // 模板来源
         template: 'public/index.html',
+        // 输出文件名
         filename: 'index.html'
       }
     },
